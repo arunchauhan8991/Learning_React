9c242e4967d263dac5c15d789fa9e393a5a72b6a
arunchauhan8991 <arunchauhan8991@gmail.com>
2025-06-27 18:33:46 +0530

    state updates are asynchronous and batched

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -8,8 +8,20 @@ function App() {
 
 
   const addValue = () =>{
-    if(counter > 19) return
-    setCounter(counter+1)
+    if (counter > 19) return;
+
+    //State updates are [asynchronous] and batched
+
+    //All lines use the same old value of counter — they don’t wait for the first setCounter to finish before executing the next one.
+
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
+    setCounter((prevCounter) => prevCounter + 1);
   }
 
   const removeValue = () => {
