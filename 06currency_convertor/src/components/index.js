565af74c2ed88ec4710ad9aaa8b30682142faaad
arunchauhan8991 <arunchauhan8991@gmail.com>
2025-07-01 18:33:56 +0530

    add currency convertor

---
+++ 06currency_convertor/src/components/index.js
@@ -0,0 +1,5 @@
+// in large projects do this import all components here and then export all from here in this way u just need to import index.js at app.jsx
+
+import InputBox from "./InputBox";
+
+export {InputBox}
--- 06currency_convertor/src/hooks/useCurrencyInfo.js
+++ 06currency_convertor/src/hooks/useCurrencyInfo.js
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency){
+    const [data, setData] = useState({});
     useEffect(() => {
+
+
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then()
-    }, [])
+        .then((res) => res.json())   //is .then se api ka response json me convert hoga asynchronously (api ka promise fulfill hoga)
+        .then((data) => setData(data[currency]))
+        console.log(data);
+        
+    }, [currency]);
+    return data
 }
+
+export default useCurrencyInfo; 
